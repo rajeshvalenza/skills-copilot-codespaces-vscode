92304ec13ffb21f58acdba5f7b5277f798841d8f
rajeshvalenza <97225582+rajeshvalenza@users.noreply.github.com>
2024-06-15 06:49:46 +0000

    Copilot third commit

--- comments.js
+++ comments.js
@@ -1,31 +1,32 @@
-// create web server
-const express = require("express");
-const app = express();
-const path = require("path");
-const fs = require("fs");
-const bodyParser = require("body-parser");
+// Create web server
+// create a server
+// create a server
+var express = require('express');
+var app = express();
+var bodyParser = require('body-parser');
+var fs = require('fs');
+var comments = require('./comments.json');
 
-// set up the express server
-app.use(bodyParser.urlencoded({ extended: false }));
+// Set up the server
+app.use(express.static(__dirname + '/public'));
 app.use(bodyParser.json());
-app.use(express.static("public"));
+app.use(bodyParser.urlencoded({ extended: true }));
 
-// get the comments
-app.get("/comments", (req, res) => {
-  res.sendFile(path.join(__dirname, "comments.json"));
+// Get the comments
+app.get('/comments', function(req, res) {
+  res.send(comments);
 });
 
-// post the comments
-app.post("/comments", (req, res) => {
-  console.log(req.body);
-  let comments = JSON.parse(
-    fs.readFileSync(path.join(__dirname, "comments.json"))
-  );
-  comments.push(req.body);
-  fs.writeFileSync(path.join(__dirname, "comments.json"), JSON.stringify(comments));
-  res.send("comments.json updated");
+// Add a comment
+app.post('/comments', function(req, res) {
+  var comment = req.body;
+  comments.push(comment);
+  fs.writeFile('comments.json', JSON.stringify(comments, null, 4), function(err) {
+    res.send(comment);
+  });
 });
 
-app.listen(3000, () => {
-  console.log("Server is listening on port 3000");
+// Start the server
+app.listen(3000, function() {
+  console.log('Server is running on port 3000');
 });
